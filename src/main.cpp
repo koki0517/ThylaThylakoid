2c43ff056e6a591f1014499251a32bb3fdbfb348
Koki <89891689+koki0517@users.noreply.github.com>
2023-07-31 20:30:39 +0900

    とふとふ

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,17 @@
 #include "HardwareSerial.h"
 #include <Wire.h>
 #include <SparkFun_VL53L5CX_Library.h>
+#include "VL53L5CX.h"
+
+SparkFun_VL53L5CX myImager_left;
+VL53L5CX_ResultsData measurementData_left; // Result data class structure, 1356 byes of RAM
+
+SparkFun_VL53L5CX myImager_right;
+VL53L5CX_ResultsData measurementData_right; // Result data class structure, 1356 byes of RAM
+
+VL53L5CX tof_left(0x10, null, myImager_left, measurementData_left);
+VL53L5CX tof_right(0x11, null, myImager_right, measurementData_right);
+
 
 void setup(){
   Serial.begin(1115200);
